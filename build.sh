16ddea6363a67ddcd76bd23946271fa33f590a52
cxynm <aa843514778@qq.com>
2020-11-29 21:07:32 +0800

    init

--- build.sh
+++ build.sh
@@ -6,32 +6,22 @@ echo "开始构建:::"
 
 yarn build
 
-echo "开始上传：：："
+echo "开始复制：：："
 
-git init
+cp -f docs/.vuepress/dist/* ../CCblog
+
+echo "复制完成:::"
+
+echo "开始上传:::"
+
+cd ../CCblog
 
 git add .
 
-git commit -m 'build'
+git commit -m 'save:::ok'
 
-git remote add origin git@github.com:chenxuyaoniming/CCblog.git
+git push origin release
 
-git remote -v
-
-git fetch
-
-git rebase origin/main
-
-git push origin main:main --force
-
-# git init
-# git add .
-# git commit -m "build"
-# git branch -M main
-# git remote add origin git@github.com:chenxuyaoniming/CCblog.git
-# git push origin --delete main
-# git push -u origin main
-
-echo "上传完陈：：："
+echo "上传完成：：："
 
 cd -
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,31 @@
 
 
+const basePath = process.env.NODE_ENV === 'production' ? '/CCblog/' : '/'
+
 module.exports = {
     title: 'TTblog',
     description: '努力！奋斗！',
-    base: '/CCblog/'
+    base: basePath,
+    themeConfig: {
+        nav: [
+            {
+                'text':'个人介绍',
+                link: '/aboutMe'
+            }, {
+                text: '学习',
+                link: 'teach'
+            }
+        ],
+        sidebar: [
+            '/',
+            {
+                title: 'css学习',
+                path: '/css',
+                children: [
+                    '/css/',
+                    '/css/bfc'
+                ]
+            }
+        ]
+    }
 }
